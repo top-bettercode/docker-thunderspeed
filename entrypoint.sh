eebb051d9a62474283ac8a9b28be8368d3d41760
Peter Wu <piterwu@outlook.com>
2019-09-21 14:02:07 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,11 @@ if [ -d "/home/thunderspeed/.thunderspeed" ]; then
     chown thunderspeed:thunderspeed /home/thunderspeed/.thunderspeed
 fi
 
+if [ "$CRACKED" == "true" ] && [ ! -d "/home/thunderspeed/.deepinwine/Deepin-ThunderSpeed/drive_c/Program Files/Thunder Network/Thunder/Program/XLUserS.dll" ] && [ -d "/home/thunderspeed/.deepinwine/Deepin-ThunderSpeed/drive_c/Program Files/Thunder Network/Thunder/Program/" ]; then
+    echo 'crack thunderspeed'
+    cp /home/thunderspeed/dll/* "/home/thunderspeed/.deepinwine/Deepin-ThunderSpeed/drive_c/Program Files/Thunder Network/Thunder/Program/"
+fi
+
 su thunderspeed <<EOF
 echo "启动 $APP"
 "/opt/deepinwine/apps/Deepin-$APP/run.sh"
