c9b87484011904204bb782a2fa6eb1bf1e10729b
Peter Wu <piterwu@outlook.com>
2021-01-15 21:57:28 +0800

    支持帐户登录信息目录映射

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,26 @@ if [ $UID != $(echo `id -u thunderspeed`) ]; then
 fi
 chown thunderspeed:thunderspeed "/迅雷下载"
 if [ -d "/home/thunderspeed/.thunderspeed" ]; then
-    chown thunderspeed:thunderspeed /home/thunderspeed/.thunderspeed
-fi
-
-if [ "$CRACKED" == "true" ] && [ ! -d "/home/thunderspeed/.deepinwine/Deepin-ThunderSpeed/drive_c/Program Files/Thunder Network/Thunder/Program/XLUserS.dll" ] && [ -d "/home/thunderspeed/.deepinwine/Deepin-ThunderSpeed/drive_c/Program Files/Thunder Network/Thunder/Program/" ]; then
-    echo 'crack thunderspeed'
-    cp /home/thunderspeed/dll/* "/home/thunderspeed/.deepinwine/Deepin-ThunderSpeed/drive_c/Program Files/Thunder Network/Thunder/Program/"
+    chown thunderspeed:thunderspeed  /home/thunderspeed/.thunderspeed
 fi
 
 su thunderspeed <<EOF
 echo "启动 $APP"
+if [ ! -d "/home/thunderspeed/.deepinwine/Deepin-ThunderSpeed/" ]; then
+    "/opt/deepinwine/apps/Deepin-$APP/run.sh" -c
+    if [ "$CRACKED" == "true" ]; then
+        echo 'crack thunderspeed'
+        cp /home/thunderspeed/dll/* "/home/thunderspeed/.deepinwine/Deepin-ThunderSpeed/drive_c/Program Files/Thunder Network/Thunder/Program/"
+    fi
+    if [ -d "/home/thunderspeed/.deepinwine/Deepin-ThunderSpeed/drive_c/Program Files/Thunder Network/Thunder/Profiles" ]; then
+        if [ ! -d "/home/thunderspeed/.thunderspeed/Community" ]; then
+            mv "/home/thunderspeed/.deepinwine/Deepin-ThunderSpeed/drive_c/Program Files/Thunder Network/Thunder/Profiles/*" "/home/thunderspeed/.thunderspeed/"
+        fi
+        rm -rf "/home/thunderspeed/.deepinwine/Deepin-ThunderSpeed/drive_c/Program Files/Thunder Network/Thunder/Profiles"
+    else
+        mkdir -p "/home/thunderspeed/.deepinwine/Deepin-ThunderSpeed/drive_c/Program Files/Thunder Network/Thunder"
+    fi
+    ln -s /home/thunderspeed/.thunderspeed   "/home/thunderspeed/.deepinwine/Deepin-ThunderSpeed/drive_c/Program Files/Thunder Network/Thunder/Profiles"
+fi
 "/opt/deepinwine/apps/Deepin-$APP/run.sh"
 EOF
